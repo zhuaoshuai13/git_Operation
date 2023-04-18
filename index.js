59b2a590118de478d73e9768954ead6278f57a6d
zhuaoshuai13 <531024850@qq.com>
2023-04-18 16:39:25 +0800

    feat: 需求2

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ const Require2 = () => {
   console.log('Require2');
   console.log('调整');
 };
+
+const Require3 = () => {
+  console.log('Require3');
+};
+
+const Require4 = () => {
+  console.log('Require5');
+  console.log('aaa');
+};
+
+const Require5 = () => {
+  console.log('a');
+  console.log('Require5');
+};
+
+const Require6 = () => {
+  console.log('Require6');
+};
